00893b608cfab03ad0ba7fed54f410753c1ff27b
Juan Pablo Pinzon <juanpablopinzon3717@gmail.com>
2022-12-04 13:51:10 -0500

    Please enter the commit message for your changes. Lines starting

--- functions.js
+++ functions.js
@@ -1,22 +1,28 @@
 function oddAndEven() {
     let sentence = document.getElementById("input").value;
-    let splitSentence = sentence.split(" ");
+    let splitSentence = sentence.split(" ").join();
     console.log(splitSentence);
     let newElement = document.createElement("div");
     let newContent = document.createTextNode(splitSentence);
     newElement.appendChild(newContent);
-    document.body.appendChild(newElement);
+    let data = document.body.appendChild(newElement).toString();
 
 }
+
+// function reset() {
+
+// }
+
+let processor = document.getElementById("button");
+processor.addEventListener("click", oddAndEven);
+
+// let reseter = document.getElementById("reset");
+// reseter.addEventListener("click", reset);
 
 function color() {
     let color = document.getElementById("colorText").value;
     document.body.style.backgroundColor = color;
 }
-
-
-let processor = document.getElementById("button");
-processor.addEventListener("click", oddAndEven);
 
 let backgroundColorChanger = document.getElementById("colorButton");
 backgroundColorChanger.addEventListener("click", color);
