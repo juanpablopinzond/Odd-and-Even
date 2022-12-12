f440673b4a99d97714eccb350bdc0b80b94a8fe5
Juan Pablo Pinzon <juanpablopinzon3717@gmail.com>
2022-12-11 20:26:43 -0500

    colored words

--- functions.js
+++ functions.js
@@ -1,28 +1,38 @@
 function oddAndEven() {
     let sentence = document.getElementById("input").value;
-    let splitSentence = sentence.split(" ").join();
+    let splitSentence = sentence.split(" ");
     console.log(splitSentence);
-    let newElement = document.createElement("div");
-    let newContent = document.createTextNode(splitSentence);
-    newElement.appendChild(newContent);
-    let data = document.body.appendChild(newElement).toString();
-
+    let coloredwords = [];
+    for (let i = 0; i < splitSentence.length; i++) {
+        let newElement = document.createElement("div");
+        let newContent = document.createTextNode(splitSentence);
+        newElement.appendChild(newContent);
+        let data = document.body.appendChild(newElement);
+        data.innerHTML = (splitSentence[i]);
+        if (i % 2 === 0) {
+            newElement.className = "red";
+            `<span style=".red">` + splitSentence + `</span>`;
+        } else {
+            newElement.className = "blue";
+            `<span style=".blue">` + splitSentence + `</span>`;
+        }
+    }
 }
 
-// function reset() {
-
-// }
-
-let processor = document.getElementById("button");
-processor.addEventListener("click", oddAndEven);
-
-// let reseter = document.getElementById("reset");
-// reseter.addEventListener("click", reset);
+function reset() {
+    let erase = document.querySelectorAll("red, blue").innerHTML = " ";
+}
 
 function color() {
     let color = document.getElementById("colorText").value;
     document.body.style.backgroundColor = color;
 }
+
+let processor = document.getElementById("button");
+processor.addEventListener("click", oddAndEven);
+
+let reseter = document.getElementById("reset");
+reseter.addEventListener("click", reset);
 
 let backgroundColorChanger = document.getElementById("colorButton");
 backgroundColorChanger.addEventListener("click", color);
